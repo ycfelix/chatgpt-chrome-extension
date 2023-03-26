5ed5f26ea7093e3f89e998576183e67d27149acc
GAYTUBER\felixyau <ycfelix7@gmail.com>
2023-03-26 17:57:17 +0800

    WIP: can communicate with server

---
+++ extension/background.js
@@ -0,0 +1,36 @@
+// Create a context menu item
+chrome.contextMenus.create({
+  id: "ask-chatgpt",
+  title: "Ask ChatGPT",
+  contexts: ["all"],
+});
+
+let loaded = false;
+
+// Listen for when the user clicks on the context menu item
+chrome.contextMenus.onClicked.addListener((info, tab) => {
+  if (!loaded) {
+    chrome.scripting
+      .executeScript({
+        target: { tabId: tab.id, allFrames: true },
+        files: ["content.js"],
+      })
+      .then(() => {
+        console.log("script injected in all frames");
+        loaded = true;
+      })
+      .then(() => {
+        if (info.menuItemId === "ask-chatgpt") {
+          // Send a message to the content script
+          console.log("asking chatgpt init");
+          chrome.tabs.sendMessage(tab.id, { type: "SHOW_UI" });
+        }
+      });
+  } else {
+    if (info.menuItemId === "ask-chatgpt") {
+      // Send a message to the content script
+      console.log("asking chatgpt");
+      chrome.tabs.sendMessage(tab.id, { type: "SHOW_UI" });
+    }
+  }
+});
